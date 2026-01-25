0571987e35d4f05146c49112974542137ac34127
mariammohamedd2 <141452298+mariamnabeh@users.noreply.github.com>
2026-01-25 08:12:26 +0200

    Add binary search solution and organize topic files
    Implemented a binary search solution in ITMO_Q2.cpp. Renamed binary_search_explained.py.cpp to STLS.CPP and moved it to Topics_summary.cpp. Added new placeholder files for Static Range Queries, Time Complexity, and binary search explanation in Topics_summary.cpp for better topic organization.

---
+++ Binary_Search/ITMO_Q2.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+using namespace std;
+
+#define el "\n"
+typedef long long ll;
+typedef long double ld;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+
+    vector<ll> arr(n);
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    while(k--){
+        ll num;
+        cin >> num;
+
+        ll l = 0, r = n - 1;
+        bool found = false;
+
+        while(l <= r){
+            ll mid = (l + r) / 2;
+
+            if(arr[mid] == num){
+                found = true;
+                break;
+            }
+            else if(arr[mid] < num){
+                l = mid + 1;
+            }
+            else{
+                r = mid - 1;
+            }
+        }
+
+        if(found)
+            cout << "YES" << el;
+        else
+            cout << "NO" << el;
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+
+    return 0;
+}
